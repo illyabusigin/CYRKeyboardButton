666ca91a52852535b8fd194ca6a71701fa6ace33
Illya Busigin <illya@cyrillian.com>
2014-07-31 21:42:50 -0500

    Configuring sample app

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -11,11 +11,16 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSMutableArray *keyboardButtons;
+@property (nonatomic, strong) UIInputView *numberView;
+
 @property (nonatomic, weak) IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
+
+#pragma mark - UIViewController
 
 - (void)viewDidLoad
 {
@@ -23,44 +28,88 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     NSArray *keys = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"0"];
+    self.keyboardButtons = [NSMutableArray arrayWithCapacity:keys.count];
     
-    UIInputView *numberView = [[UIInputView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 45) inputViewStyle:UIInputViewStyleKeyboard];
+    self.numberView = [[UIInputView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 45) inputViewStyle:UIInputViewStyleKeyboard];
     
-//    QEDKeyboardButton *aButton = [[QEDKeyboardButton alloc] initWithFrame:CGRectMake(95, 25, 60, 60) input:@"Q" inputOptions:@[@"A", @"B", @"C"]];
-//    aButton.textInput = self.textField;
-//    
-//    [numberView addSubview:aButton];
+    [keys enumerateObjectsUsingBlock:^(NSString *keyString, NSUInteger idx, BOOL *stop) {
+        CYRKeyboardButton *keyboardButton = [CYRKeyboardButton new];
+        keyboardButton.translatesAutoresizingMaskIntoConstraints = NO;
+        keyboardButton.input = keyString;
+        keyboardButton.inputOptions = @[@"A", @"B", @"C", @"D"];
+        keyboardButton.textInput = self.textField;
+        [self.numberView addSubview:keyboardButton];
+        
+        [self.keyboardButtons addObject:keyboardButton];
+    }];
     
-//    QEDKeyboardButton *zButton = [[QEDKeyboardButton alloc] initWithFrame:CGRectMake(3 + (320 - 32), 5, 26, 40) input:@"P" inputOptions:@[@"A", @"B", @"C"]];
-//    QEDKeyboardButton *tButton = [[QEDKeyboardButton alloc] initWithFrame:CGRectMake(3 + 128, 5, 26, 40) input:@"T" inputOptions:@[@"A", @"B", @"C"]];
-//    
-//    //[numberView addSubview:aButton];
-//    [numberView addSubview:zButton];
-//    [numberView addSubview:tButton];
+    [self updateConstraintsForOrientation:self.interfaceOrientation];
     
-    self.textField.inputAccessoryView = numberView;
+    self.textField.inputAccessoryView = self.numberView;
     
-    CYRKeyboardButton *newKeyboardButton = [[CYRKeyboardButton alloc] initWithFrame:CGRectMake(3, 5, 26, 40)];
-    newKeyboardButton.input = @"Q";
-    newKeyboardButton.textInput = self.textField;
-    [numberView addSubview:newKeyboardButton];
-    
-    CYRKeyboardButton *newKeyboardButton2 = [[CYRKeyboardButton alloc] initWithFrame:CGRectMake(100, 5, 26, 40)];
-    newKeyboardButton2.input = @"S";
-    newKeyboardButton2.inputOptions = @[@"s", @"ß", @"ś", @"š"];
-    newKeyboardButton2.textInput = self.textField;
-    [numberView addSubview:newKeyboardButton2];
-    
-    CYRKeyboardButton *newKeyboardButton3 = [[CYRKeyboardButton alloc] initWithFrame:CGRectMake(291, 5, 26, 40)];
-    newKeyboardButton3.input = @"P";
-    newKeyboardButton3.textInput = self.textField;
-    [numberView addSubview:newKeyboardButton3];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self updateConstraintsForOrientation:toInterfaceOrientation];
+}
+
+#pragma mark - Constraint Management
+
+- (void)updateConstraintsForOrientation:(UIInterfaceOrientation)orientation
+{
+    // Remove any existing constraints
+    [self.numberView removeConstraints:self.numberView.constraints];
+    
+    // Create our constraints
+    NSMutableDictionary *bindings = [NSMutableDictionary dictionary];
+    NSMutableString *visualFormatConstants = [NSMutableString string];
+    NSDictionary *metrics = nil;
+    
+    // Setup our metrics based on orientation
+    if (UIInterfaceOrientationIsPortrait(orientation)) {
+        metrics = @{
+                    @"margin" : @(3),
+                    @"spacing" : @(6)
+                    };
+    } else {
+        metrics = @{
+                    @"margin" : @(22),
+                    @"spacing" : @(5)
+                    };
+    }
+    
+    // Build the visual format string
+    [self.keyboardButtons enumerateObjectsUsingBlock:^(CYRKeyboardButton *button, NSUInteger idx, BOOL *stop) {
+        NSString *binding = [NSString stringWithFormat:@"keyboardButton%i", idx];
+        [bindings setObject:button forKey:binding];
+        
+        if (idx == 0) {
+            [visualFormatConstants appendString:[NSString stringWithFormat:@"H:|-margin-[%@]", binding]];
+        } else if (idx < self.keyboardButtons.count - 1) {
+            [visualFormatConstants appendString:[NSString stringWithFormat:@"-spacing-[%@]", binding]];
+        } else {
+            [visualFormatConstants appendString:[NSString stringWithFormat:@"-spacing-[%@]-margin-|", binding]];
+        }
+    }];
+    
+    // Apply horizontal constraints
+    [self.numberView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:visualFormatConstants options:0 metrics:metrics views:bindings]];
+    
+    // Apply vertical constraints
+    [bindings enumerateKeysAndObjectsUsingBlock:^(NSString *binding, id obj, BOOL *stop) {
+        NSString *format = [NSString stringWithFormat:@"V:|-6-[%@]|", binding];
+        [self.numberView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:format options:0 metrics:nil views:bindings]];
+    }];
+    
+    // Add width constraint
+    [self.keyboardButtons enumerateObjectsUsingBlock:^(CYRKeyboardButton *button, NSUInteger idx, BOOL *stop) {
+        if (idx > 0) {
+            CYRKeyboardButton *previousButton = self.keyboardButtons[idx - 1];
+            
+            [self.numberView addConstraint:[NSLayoutConstraint constraintWithItem:button attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:previousButton attribute:NSLayoutAttributeWidth multiplier:1 constant:0]];
+        }
+    }];
 }
 
 @end
